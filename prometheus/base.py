a00d77cced7f424fe6490577678c970bb3ea87d2
theunkn0wn1 <thHunkn0WNd@gmail.com>
2017-04-04 17:34:06 -0700

    changed serialTest to be a submodule to be implemented via base.py

---
+++ prometheus/base.py
@@ -0,0 +1,15 @@
+import platform # for version check
+import sys
+import os
+#import importlib #lovely name, wrapper for importing other py files
+scriptPath = "python ../serial/mySerial.py"
+os.system(scriptPath)
+class prometheus():
+	"""base class for prometheus"""
+	#begin auto-generated stub...
+	def __init__(self, arg):
+		super(prometheus, self).__init__()
+		self.arg = arg
+	#end auto-generated stub...
+	def placeHolder(self):
+		pass
