be602776c56c1695a90f8d9bc1bb2eea7fea0bae
theunkn0wn1 <thHunkn0WNd@gmail.com>
2017-04-27 10:57:14 -0700

    Began work implementing sonar sensors

--- utils/sonar.py
+++ utils/sonar.py
@@ -1,7 +1,27 @@
-class sonar():
+###
+# File: sonar.py
+# function: act as an interface with a HC-SR04 Ultrasonic
+##
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BCM) #sets pin numbering
+def isArray(obj):
+	"""tests if obj is a list"""
+	if(type(obj)==list):
+		return(1)
+	else:
+		raise TypeError("Function requires list.")
+
+class sonar(trigger,echo):
 	"""docstring for sonar"""
 	def __init__(self, arg):
 		super(sonar, self).__init__()
 		self.arg = arg
-	def myFunction(self):
-		pass#placeholder
+		if(isArray(trigger) and isArray(echo)):
+			self.pinTrigger = trigger #must be arrays!
+			self.pinEcho = echo #must be arrays!
+		def setup(self):
+			for pin in self.pinTrigger:#set up each trigger pin
+				GPIO.setup(pin,GPIO.OUT)
+			for pin in self.pinEcho:#set up each echo pin
+				GPIO.setup(pin,GPIO.IN)
