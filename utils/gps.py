9f3d0fccb09690e2565c539cbde75c76bc04febe
theunkn0wn1 <theunkn0wn1@users.noreply.github.com>
2017-04-27 19:30:56 -0700

    Began fleshing out utils/gps.py Also added a neccessary bash script to make GPS work.

---
+++ utils/gps.py
@@ -0,0 +1,10 @@
+# # #
+# File: gps.py
+# Function: handle GPS communications.
+# #
+
+class gps():
+	"""handle GPS communications."""
+	def __init__(self,arg):
+		super(gps,self).__init()
+		self.arg = arg
