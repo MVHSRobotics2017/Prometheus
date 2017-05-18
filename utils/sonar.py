c672c84ed5fd4115d8e9d7d0604d9ddb4e579642
Joshua Salzedo <theunkn0wn1@users.noreply.github.com>
2017-05-17 17:00:27 -0700

    Began reworing sonar.sonar() to act as an expandable function

--- utils/sonar.py
+++ utils/sonar.py
@@ -1,32 +1,33 @@
+
+import RPi.GPIO as GPIO
+import time
 def sonar(TRIG,ECHO):
-	import RPi.GPIO as GPIO
-	import time
-
+	"""accepts int arrays TRIG and ECHO"""
+	ret = [0.0]*len(TRIG)
+	if (len(TRIG)!=len(ECHO)):
+		raise ValueError("TRIG != ECHO!")
 	GPIO.setmode(GPIO.BOARD)
-
-	#TRIG = 7
-	#ECHO = 12
-
-	GPIO.setup(TRIG,GPIO.OUT)
-	GPIO.output(TRIG,0)
-
-	GPIO.setup(ECHO,GPIO.IN)
+	for pin in TRIG:
+		GPIO.setup(TRIG,GPIO.OUT)
+		GPIO.output(TRIG,0)
+	for pin in ECHO
+		GPIO.setup(pin,GPIO.IN)
 
 	time.sleep(0.1)
-
-	print("Starting Measurement...")
-
-	GPIO.output(TRIG,1)
-	time.sleep(0.00001)
-	GPIO.output(TRIG,0)
-
-	while GPIO.input(ECHO) == 0:
-		pass
-	start = time.time()
-	while GPIO.input(ECHO) == 1:
-		pass
-	stop = time.time()
-
-	print (stop - start) * 17000
-
+	print("Starting Measurements...")
+	for x in range(0,len(TRIG)):
+		print("beggining measurement on sensor:{i}".format(i=x))
+		GPIO.output(TRIG[x],1)
+		time.sleep(0.00001)
+		GPIO.output(TRIG[x],0)
+		while GPIO.input(ECHO) == 0:
+			pass
+		start = time.time()
+		while GPIO.input(ECHO) == 1:
+			pass
+		stop = time.time()
+		print("Mark!\tmeasured time= {delta}".format(delta=(stop-start)))
+		ret[x] = stop - start
+	#print (stop - start) * 17000
+	print("adjusted = {adjRet}".format(adjRet = ret*1.7E4))
 	GPIO.cleanup()
